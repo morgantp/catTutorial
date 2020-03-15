083d8724ef10da74f9a633238b320956f15b44be
Morgan <morgan.thomas-price@students.plymouth.ac.uk>
2020-03-15 15:58:47 +0000

    trying to fix images

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Write your JS in here
 var pics = [
-	"imgs/kitty_bed.jpg",
-	"imgs/kitty_basket.jpg", 
+	"imgs/kitty_bed.JPG",
+	"imgs/kitty_basket.JPG", 
 	"imgs/kitty_laptop.jpg",
 	"imgs/kitty_door.jpg",
-	"imgs/kitty_sink.jpg",
+	"imgs/kitty_sink.JPG",
 	"imgs/kitty_wall.jpg"
 ]
 
